7f937fe305c9e7dea26d9edc4629ab9ecf232501
SODEX42 <107519751+SODEX42@users.noreply.github.com>
2022-07-06 00:57:22 +0100

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- *  * main - Prints the alphabet in lowercase.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char letter;
+	int n;
 
-			for (letter = 'a'; letter <= 'z'; letter++)
-						putchar(letter);
-
-				putchar('\n');
-
-					return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
